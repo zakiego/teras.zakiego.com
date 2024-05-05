64c2b4f5223f20b220f6fa897e4eaa9db0c182f7
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-05-05 08:26:17 +0800

    feat: add remove empty message

--- server/api/history.ts
+++ server/api/history.ts
@@ -1,7 +1,11 @@
-import { asc, desc } from "drizzle-orm";
+import { asc, desc, eq } from "drizzle-orm";
 import { dbClient, dbSchema } from "~/database/drizzle";
 
 export default defineEventHandler(async (event) => {
+  // const deleteEmpty = await dbClient
+  //   .delete(dbSchema.history)
+  //   .where(eq(dbSchema.history.message, ""));
+
   const data = await dbClient.query.history.findMany({
     orderBy: desc(dbSchema.history.timestamp),
     limit: 200,
